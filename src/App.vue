1a17fc9064e4e7fafcb5988a8201f14cf0e2d813
CleiverSoares <cleiversoares2@gmail.com>
2024-04-22 14:00:51 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+<IndicacaoPacote/>
+  </div>
+</template>
+
+<script>
+import IndicacaoPacote from './components/indicacaoPacote.vue';
+
+  export default {
+    components: {IndicacaoPacote}
+  }
+</script>
+
+<style scoped>
+
+</style>
